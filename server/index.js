935e3536bf6da6d4d52368ffe455c4667d35649d
antonyjes <72809149@continental.edu.pe>
2023-04-14 23:36:17 -0500

    Register Admin API

--- server/index.js
+++ server/index.js
@@ -1,1 +1,55 @@
-console.log("Starting server");
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from "uuid";
+import { registerAdmin } from "./controllers/auth.js";
+
+//CONFIGURATIONS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+mongoose.set("strictQuery", true);
+
+//FILE STORAGE
+const adminStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets/admins");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + uuidv4();
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const adminUpload = multer({ storage: adminStorage });
+
+//ROUTES WITH FILES
+app.post("/auth/registerAdmin", adminUpload.single("picture"), registerAdmin);
+
+//MOONGOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+  })
+  .catch((error) => console.log(error));
