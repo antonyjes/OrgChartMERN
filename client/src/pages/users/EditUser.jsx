25fa09d638692c5ed25080c4595301da36092740
antonyjes <72809149@continental.edu.pe>
2023-04-20 11:35:51 -0500

    Edit User component

---
+++ client/src/pages/users/EditUser.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux"
+import { useParams } from "react-router-dom";
+import Sidebar from "../../components/Sidebar";
+import Aside from "../../components/Aside";
+
+const EditUser = () => {
+    const token = useSelector((state) => state.token);
+    const {userId} = useParams();
+    const [userData, setUserData] = useState(null);
+
+    const getUser = async (req, res) => {
+        const response = await fetch(`http://localhost:3003/users/${userId}/user`, {
+            method: "GET",
+            headers: {Authorization: `Bearer ${token}`},
+        });
+
+        const data = await response.json();
+        setUserData(data);
+    };
+
+    useEffect(() => {
+        getUser();
+    }, []); // eslint-disable-line
+
+    if (!userData) return null;
+
+    return(
+        <>
+            <Sidebar />
+            <Aside />
+        </>
+    )
+};
+
+export default EditUser;
